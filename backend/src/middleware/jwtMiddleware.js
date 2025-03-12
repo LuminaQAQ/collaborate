07ad53d65a84fad080250dce106073f5253f308d
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-03-12 15:33:23 +0800

    temp commit

--- backend/src/middleware/jwtMiddleware.js
+++ backend/src/middleware/jwtMiddleware.js
@@ -1,3 +1,12 @@
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {import("express").NextFunction} next 
+ */
 const jwtMiddleware = (req, res, next) => {
-    
+    const token = req.headers["authorization"]
+    console.log(req.headers);
 }
+
+module.exports = jwtMiddleware;
