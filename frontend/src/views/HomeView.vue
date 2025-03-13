8332ad6cff6347adc65bbaa9627121ead80b0ec6
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-03-14 00:16:17 +0800

    feat(frontend): 搭建首页基本布局

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    
-  </main>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
+<style lang="scss">
+.common-layout {
+  width: 100%;
+  height: 100%;
+
+  .el-container {
+    height: 100%;
+  }
+}
+</style>
